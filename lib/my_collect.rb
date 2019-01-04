9fa8e1376f00fbff4fa383ef0e40d280b5a13ecd
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-04 18:18:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    yield
+    new_collection.push(collection[0])
+    collection.slice(0)
+    i += 1
+  end
+  new_collection
+end
 
